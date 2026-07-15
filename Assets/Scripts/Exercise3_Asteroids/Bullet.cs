17e74f0ab7cd168bfae64de2240a78971c710881
evrstt <billycruse@gmail.com>
2026-07-15 14:00:49 -0500

    Update to Bullet.cs
    - removed assignment instructions from script.
    - added comments throughout on scripts functionality.

--- Assets/Scripts/Exercise3_Asteroids/Bullet.cs
+++ Assets/Scripts/Exercise3_Asteroids/Bullet.cs
@@ -1,40 +1,36 @@
-/*
- * Assignment: AsteroidsGame - Bullet Script - 2
- * 
- * Objective:
- * Implement a player controller for a spaceship in an Asteroids prototype. The player should be able to rotate the ship,
- * move forward, wrap around the screen, and shoot bullets. 
- * 
- * Requirements:   
- * PART 2: Shooting
- * 1. The bullets should start off moving in the direciton they are spawned at a speed set by bulletSpeed. 
- *      This should be set in the Start method of this script.
- *      The movement of the bullet should be done with Physics applied to a Rigidbody2D.
- * 2. The bullets should be destroyed after bulletLifetime seconds or when they collide with an asteroid.
- *
- */
 using UnityEngine;
 
+// Ensures that any GameObject using this script has a Ridigbody2D component.
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
+    // Controls how quickly the bullet moves.
     [SerializeField] private float bulletSpeed = 20f;
+
+    // Controls how long the bullet exists before being destroyed.
     [SerializeField] private float bulletLifetime = 5f;
 
+    // Sores a reference to the bullet's Rigidbody2D component.
     private Rigidbody2D rb;
 
     void Start()
     {
+        // Gets the Rigidbody2d attached to the bullet.
         rb =  GetComponent<Rigidbody2D>();
+
+        // Moves the bullet forward in the direciton it is facing.
         rb.linearVelocity = transform.up * bulletSpeed;
 
+        // Automatically destroys the bullet after its lifetime expires.
         Destroy(gameObject, bulletLifetime);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // Checks whether the object the bullet collided with has an Asteroid component
         if(collision.gameObject.GetComponent<Asteroid>() != null)
         {
+            // Destroys the bullet after it his an asteroid.
             Destroy(gameObject);
         }
     }
