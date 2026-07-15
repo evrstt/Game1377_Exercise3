b3ee4e17a86f665d8d8582fa966a342a07926e75
evrstt <billycruse@gmail.com>
2026-07-15 13:31:41 -0500

    Update to ScreenWrap.cs
    - added comments explaining ScreenWrap.cs functionality throughout script

--- Assets/Scripts/Exercise3_Asteroids/ScreenWrap.cs
+++ Assets/Scripts/Exercise3_Asteroids/ScreenWrap.cs
@@ -1,5 +1,10 @@
 using UnityEngine;
 
+/// <summary>
+/// Wraps a GameObject to the opposite side of the screen
+/// whenever it travels beyond the screen boundaries
+/// </summary>
+
 public class ScreenWrap : MonoBehaviour
 {
     void Update()
@@ -7,28 +12,38 @@ public class ScreenWrap : MonoBehaviour
         WrapAround();
     }
 
+    // Creates a copy of the objects current position.
     private void WrapAround()
     {
         Vector3 wrappedTransformPosition = transform.position;
 
+        // Checks whether the object has moved past the right side
+        // or left side of the screen.
         if (wrappedTransformPosition.x > ScreenBounds.screenRight)
         {
+            //Moves the object from the right side to the left side.
             wrappedTransformPosition.x = ScreenBounds.screenLeft;
         }
         else if (wrappedTransformPosition.x < ScreenBounds.screenLeft)
         {
+            //Moves the object from the left side to the right side.
             wrappedTransformPosition.x = ScreenBounds.screenRight;
         }
 
+        // Checks whether the object has moved past the top
+        // or bottom of the screen.
         if (wrappedTransformPosition.y > ScreenBounds.screenTop)
         {
+            //Moves the object from the top to the bottom.
             wrappedTransformPosition.y = ScreenBounds.screenBottom;
         }
         else if (wrappedTransformPosition.y < ScreenBounds.screenBottom)
         {
+            //Moves the object from the bottom to the top.
             wrappedTransformPosition.y = ScreenBounds.screenTop;
         }
 
+        // Applies the updated position to the GameObject.
         transform.position = wrappedTransformPosition;
     }
 }
