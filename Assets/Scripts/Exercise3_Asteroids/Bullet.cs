b05accf5bbfc45536c2b3c4ed40a0d1b8c1e4efc
evrstt <billycruse@gmail.com>
2026-07-18 11:41:37 -0500

    Update to Bullet.cs
    - added missing summary notes
    - added a constant for the Asteroid Tag
    - replaced the collision via component with collision via tag

--- Assets/Scripts/Exercise3_Asteroids/Bullet.cs
+++ Assets/Scripts/Exercise3_Asteroids/Bullet.cs
@@ -10,9 +10,15 @@ public class Bullet : MonoBehaviour
     // Controls how long the bullet exists before being destroyed.
     [SerializeField] private float bulletLifetime = 5f;
 
-    // Sores a reference to the bullet's Rigidbody2D component.
+    // Stores a reference to the bullet's Rigidbody2D component.
     private Rigidbody2D rb;
 
+    // Stores the tag assigned assigned to asteroid objects
+    private const string asteroidTag = "Asteroid";
+
+    /// <summary>
+    /// Gives bullet its starting velocity and begins its lifetime timer.
+    /// </summary>
     void Start()
     {
         // Gets the Rigidbody2d attached to the bullet.
@@ -25,10 +31,14 @@ public class Bullet : MonoBehaviour
         Destroy(gameObject, bulletLifetime);
     }
 
+    /// <summary>
+    /// Destroys the bullet when it collides with an asteroid
+    /// </summary>
+    /// <param name="collision"></param>
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // Checks whether the object the bullet collided with has an Asteroid component
-        if(collision.gameObject.GetComponent<Asteroid>() != null)
+        // Checks whether the object the bullet collided with is tagged with an asteroid
+        if(collision.gameObject.CompareTag(asteroidTag))
         {
             // Destroys the bullet after it his an asteroid.
             Destroy(gameObject);
